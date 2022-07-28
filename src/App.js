8eed1c6d6a8f49111b2e868bf6f3c271e92fa203
jeison diaz palmera <jadiazp@indracompany.com>
2022-07-28 09:11:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const URL_RICK = process.env.REACT_APP_API_RICK
+
+  console.log(URL_RICK)
+
+  useEffect(() => {
+    fetch(URL_RICK)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.results)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloady.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
